98af161101e6d7e78acc4758880b9456c2bde347
Serhat Kılbaş <serhatkilbas127@gmail.com>
2024-09-05 18:12:27 +0300

    Add project adding functionality

--- src/index.js
+++ src/index.js
@@ -45,14 +45,22 @@ sidebarItems.forEach(item => sidebar.appendChild(createSidebarItem(item)));
 
 const projects = document.createElement("div");
 projects.classList.add("projects");
-projects.innerHTML = '<span>My Projects</span><i class="fa-regular fa-plus fa-sm"></i>';
+projects.innerHTML = '<span>My Projects</span><div class="add-project"><i class="fa-regular fa-plus fa-sm"></i></div>';
 sidebar.appendChild(projects);
 
-Project.GetProjectList().forEach(project => {
-    const projectItem = createSidebarItem({ class: 'project-item', icon: 'fa-folder', text: project.name });
-    sidebar.appendChild(projectItem);
-});
+const projectList = document.createElement("div");
+projectList.classList.add("project-list");
+sidebar.appendChild(projectList);
 
+RenderProjects();
+function RenderProjects() {
+    projectList.innerHTML = '';
+
+    Project.GetProjectList().forEach(project => {
+        const projectItem = createSidebarItem({ class: 'project-item', icon: 'fa-folder', text: project.name });
+        projectList.appendChild(projectItem);
+    });
+}
 const addTaskModal = document.createElement("div");
 addTaskModal.classList.add("add-task-modal");
 addTaskModal.innerHTML = `
@@ -77,12 +85,17 @@ addTaskModal.innerHTML = `
                     <option value="none">Select Project</option>
                     ${Project.GetProjectList().map(project => `<option value="${project.name}">${project.name}</option>`).join('')}
                 </select>
-                <button class="modal-cancel-button cancel-add-button" onclick="document.querySelector('.add-task-modal').style.display='none'">Cancel</button>
+                <button class="modal-cancel-button cancel-add-button"">Cancel</button>
                 <button class="modal-submit-button add-button">Add Task</button>
             </div>
         </form>
     </div>
 `;
+
+function closeAddTaskModal(event) {
+    event.preventDefault();
+    addTaskModal.style.display = 'none';
+}
 
 const updateTaskModal = document.createElement("div");
 updateTaskModal.classList.add("update-task-modal");
@@ -108,15 +121,47 @@ updateTaskModal.innerHTML = `
                     <option value="none">Select Project</option>
                     ${Project.GetProjectList().map(project => `<option value="${project.name}">${project.name}</option>`).join('')}
                 </select>
-                <button class="modal-cancel-button cancel-update-button" onclick="document.querySelector('.update-task-modal').style.display='none'">Cancel</button>
+                <button class="modal-cancel-button cancel-update-button"">Cancel</button>
                 <button class="modal-submit-button update-button">Update Task</button>
             </div>
         </form>
     </div>
 `;
 
+function closeUpdateTaskModal(event) {
+    event.preventDefault();
+    updateTaskModal.style.display = 'none';
+}
+
+const addProjectModal = document.createElement("div");
+addProjectModal.classList.add("add-project-modal");
+addProjectModal.innerHTML = `
+    <div class="modal-content">
+        <form id="add-project-form">
+            <input type="text" id="add-project-name" name="project-name" placeholder="Project name" maxlength="10" required>
+            <button class="modal-cancel-button cancel-add-project-button">Cancel</button>
+            <button class="modal-submit-button add-project-button">Add Project</button>
+        </form>
+    </div>
+`;
+
+function closeAddProjectModal(event) {
+    event.preventDefault();
+    addProjectModal.style.display = 'none';
+}
+
 document.body.appendChild(addTaskModal);
 document.body.appendChild(updateTaskModal);
+document.body.appendChild(addProjectModal);
+
+const cancelAddTaskButton = document.querySelector('.cancel-add-button');
+cancelAddTaskButton.addEventListener('click', closeAddTaskModal);
+
+const cancelUpdateTaskButton = document.querySelector('.cancel-update-button');
+cancelUpdateTaskButton.addEventListener('click', closeUpdateTaskModal);
+
+const cancelAddProjectButton = document.querySelector('.cancel-add-project-button');
+cancelAddProjectButton.addEventListener('click', closeAddProjectModal);
 
 let currentView = 'today';
 
@@ -124,11 +169,12 @@ const viewHandlers = {
     'inbox': loadInbox,
     'today': loadTodayTasks,
     'this-week': loadThisWeekTasks,
-    'all-tasks': loadAllTasks
+    'all-tasks': loadAllTasks,
 };
 
 sidebar.addEventListener('click', (e) => {
     const item = e.target.closest('.sidebar-item');
+
     if (item && !item.classList.contains('add-task')) {
         document.querySelectorAll('.sidebar-item').forEach(i => i.classList.remove('active'));
         item.classList.add('active');
@@ -136,6 +182,9 @@ sidebar.addEventListener('click', (e) => {
         if (view) {
             currentView = view;
             viewHandlers[view]();
+        } else {
+            currentView = item.querySelector('span').textContent;            
+            loadProjectTasks(item.textContent.trim());
         }
     }
 });
@@ -153,22 +202,30 @@ document.addEventListener('click', (e) => {
 });
 
 function refreshCurrentView() {
+    if (!viewHandlers[currentView]) {
+        loadProjectTasks(currentView);
+        return;
+    }
     viewHandlers[currentView]();
     document.querySelector(`.${currentView}`).classList.add('active');
 }
 
-function loadTasks(container, tasks) {
+
+function loadTasks(tasks) {
+    const taskContainer = document.createElement("div");
+    taskContainer.classList.add("task-container");
+
     const incompleteCount = tasks.filter(task => !task.done).length;
 
     function updateTaskCount() {
         const incompleteCount = tasks.filter(task => !task.done).length;
-        const taskCountElement = container.querySelector('.today-task-count span');
+        const taskCountElement = taskContainer.querySelector('.task-count span');
         taskCountElement.textContent = `${incompleteCount} tasks`;
     }
 
-    container.innerHTML = `
-        <div class="today-header">${currentView.charAt(0).toUpperCase() + currentView.slice(1)}</div>
-        <div class="today-task-count">
+    taskContainer.innerHTML = `
+        <div class="task-header">${currentView.charAt(0).toUpperCase() + currentView.slice(1)}</div>
+        <div class="task-count">
             <img src="${checkedIcon}" alt="Checked Icon">           
             <span>${incompleteCount} tasks</span>
         </div>
@@ -241,55 +298,38 @@ function loadTasks(container, tasks) {
                 updateTaskModal.style.display = 'none';
             });
         })
-        container.appendChild(taskItem);
+        taskContainer.appendChild(taskItem);
     });
 
+    viewContent.innerHTML = '';
+    viewContent.appendChild(taskContainer);
     updateTaskCount();
 }
 
 function loadInbox() {
-    const inboxContainer = document.createElement("div");
-    inboxContainer.classList.add("inbox-container");
-    const inboxTasks = Task.GetTaskList().filter(task => task.projectID.trim() === 'none');
-    loadTasks(inboxContainer, inboxTasks);
-    viewContent.innerHTML = "";
-    viewContent.appendChild(inboxContainer);
+    loadTasks(Task.GetTaskList().filter(task => task.projectID.trim() === 'none'));
 }
 
 function loadTodayTasks() {
-    const todayContainer = document.createElement("div");
-    todayContainer.classList.add("today-container");
-    const today = new Date();
-    const todaysTasks = Task.GetTaskList().filter(task => {
-        const dueDate = new Date(task.dueDate);
-        return dueDate.getDay() === today.getDay();
-    });
-    loadTasks(todayContainer, todaysTasks);
-    viewContent.innerHTML = "";
-    viewContent.appendChild(todayContainer);
+    loadTasks(Task.GetTaskList().filter(task => new Date(task.dueDate).getDay() === new Date().getDay()));
 }
 
 function loadThisWeekTasks() {
-    const thisWeekContainer = document.createElement("div");
-    thisWeekContainer.classList.add("this-week-container");
-    const today = new Date();
-    const startOfWeek = new Date(today.setDate(today.getDate() - today.getDay()));
-    const endOfWeek = new Date(today.setDate(today.getDate() - today.getDay() + 6));
-    const thisWeekTasks = Task.GetTaskList().filter(task => {
+    loadTasks(Task.GetTaskList().filter(task => {
+        const today = new Date();
+        const startOfWeek = new Date(today.setDate(today.getDate() - today.getDay()));
+        const endOfWeek = new Date(today.setDate(today.getDate() - today.getDay() + 6));
         const dueDate = new Date(task.dueDate);
         return dueDate >= startOfWeek && dueDate <= endOfWeek;
-    });
-    loadTasks(thisWeekContainer, thisWeekTasks);
-    viewContent.innerHTML = "";
-    viewContent.appendChild(thisWeekContainer);
+    }));
 }
 
 function loadAllTasks() {
-    const allTasksContainer = document.createElement("div");
-    allTasksContainer.classList.add("all-tasks-container");
-    loadTasks(allTasksContainer, Task.GetTaskList());
-    viewContent.innerHTML = "";
-    viewContent.appendChild(allTasksContainer);
+    loadTasks(Task.GetTaskList());
+}
+
+function loadProjectTasks(projectName) {
+    loadTasks(Task.GetTaskList().filter(task => task.projectID.trim() === projectName));
 }
 
 refreshCurrentView();
@@ -352,5 +392,30 @@ document.addEventListener('DOMContentLoaded', () => {
         if (event.target === updateTaskModal) {
             updateTaskModal.style.display = "none";
         }
+    });
+
+    addProjectModal.addEventListener('click', (event) => {
+        if (event.target === addProjectModal) {
+            addProjectModal.style.display = "none";
+        }
+    });
+
+    const addProjectButton = document.querySelector('.add-project');
+    addProjectButton.addEventListener("click", () => {
+        addProjectModal.style.display = 'block';
+
+        const submitProjectButton = document.querySelector('.add-project-button');
+        submitProjectButton.addEventListener("click", (event) => {
+            event.preventDefault();
+            const form = document.querySelector("#add-project-form");
+            if (!form.checkValidity()) { form.reportValidity(); return; }
+            
+            const formData = new FormData(form);
+            let projectName = formData.get("project-name");
+            projectName = projectName.charAt(0).toUpperCase() + projectName.slice(1);
+            new Project(projectName);
+            RenderProjects();
+            addProjectModal.style.display = "none";
+        });
     });
 });
